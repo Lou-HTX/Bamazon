06009cdb92dc34032600d27b7a58f06e596e7c57
Lou-HTX <luismoreno.it@gmail.com>
2017-06-26 14:57:37 -0500

    removed DB credential info

--- bamazon.js
+++ bamazon.js
@@ -6,8 +6,8 @@ var prompt = require('prompt');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    user: "test",
-    password: "Welcome01",
+    user: "*****",
+    password: "*****",
     database: "Bamazon"
 });
 
